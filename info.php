8e4ccda17582a8d3494e6d6414883d56fd016271
= <=>
2019-08-20 18:38:49 +0500

    mychange

--- info.php
+++ info.php
@@ -1,6 +1,6 @@
 <?php
 echo "hello how are you";
 echo "these are the changes in the repository";
-echo "I have changed the file 25";
+echo "I have changed the file 85_br";
 echo "this is the last change";
 ?>
